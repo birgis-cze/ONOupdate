8897523923579bb54f93eb8aa2c6264cf84d5c8e
birgis-cze <birgis.htc@gmail.com>
2026-09-09 11:56:45 +0200

    xx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,5 +41,4 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     implementation("org.jsoup:jsoup:1.17.2")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("androidx.glance:glance-appwidget:1.0.0")
 }
