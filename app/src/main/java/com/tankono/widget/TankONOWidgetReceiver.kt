f606815ade4c6b347ef5eea3f68a450cf0bcf1c9
birgis-cze <birgis.htc@gmail.com>
2026-09-10 18:07:48 +0200

    0

--- app/src/main/java/com/tankono/widget/TankONOWidgetReceiver.kt
+++
@@ -1,8 +0,0 @@
-package com.tankono.widget
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class TankONOWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = TankONOWidget()
-}
