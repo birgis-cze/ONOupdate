9f41363cfcfe35f742331379a5a54516e8b3852b
birgis-cze <birgis.htc@gmail.com>
2026-09-13 05:15:28 +0200

    Release_2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cz.tankono.widget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2                         // ################### zde zvýšit pro nový release 
+        versionName = "2.0"                     // ################### zde zvýšit pro nový release 
     }
     
     lint {
