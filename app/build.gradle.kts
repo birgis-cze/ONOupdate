fcd01c5ffe2b51652511cdb382fb69597ddf591f
birgis-cze <birgis.htc@gmail.com>
2026-09-13 05:47:45 +0200

    REL_2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cz.tankono.widget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1                         // ################### zde zvýšit pro nový release 
-        versionName = "1.0"                     // ################### zde zvýšit pro nový release 
+        versionCode = 2                         // ################### zde zvýšit pro nový release 
+        versionName = "2.0"                     // ################### zde zvýšit pro nový release 
 
         // GitHub token z prostředí (v CI) nebo prázdný (lokálně)
         val ghToken = System.getenv("GH_TOKEN") ?: ""
